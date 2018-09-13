b897981448a41e58f10512daadc39445cdd2793a
ruigoncalo <ruimiguelgoncalo@gmail.com>
2018-09-13 23:32:11 +0100

    Add requestRates to interactor

--- domain/src/main/java/com/ruigoncalo/domain/RatesInteractor.kt
+++ domain/src/main/java/com/ruigoncalo/domain/RatesInteractor.kt
@@ -1,9 +1,12 @@
 package com.ruigoncalo.domain
 
 import com.ruigoncalo.domain.model.Rates
+import io.reactivex.Completable
 import io.reactivex.Observable
 
 interface RatesInteractor {
 
     fun getRates(currency: String, newValue: String): Observable<Rates>
+
+    fun requestRates(): Completable
 }
